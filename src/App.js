f19c6127e1f7bd5514fffdb4525ad98c3c611217
Chintana Meegamarachchi <chintana@appsolve.com.au>
2021-05-11 18:04:54 +0800

    Loads data from remote api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { Container } from "@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
+import TextField from "@material-ui/core/TextField";
+import axios from "axios";
+
+import InputAdornment from "@material-ui/core/InputAdornment";
+import SearchIcon from "@material-ui/icons/Search";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      "& > *": {
+        margin: theme.spacing(1),
+        width: "100%",
+      },
+    },
+    terminal: {
+      margin: theme.spacing(1),
+      backgroundColor: "#000",
+      border: "1px solid #000",
+      color: "#00ff00",
+      padding: "8px",
+      fontFamily: "courier new",
+    },
+  })
+);
 
 function App() {
+  const [input, setInput] = React.useState("https://swapi.dev/api/people/1");
+  const [url, setUrl] = React.useState("https://swapi.dev/api/people/1");
+  const [content, setContent] = React.useState("");
+
+  const handleClick = () => setUrl(input);
+
+  const classes = useStyles();
+
+  React.useEffect(() => {
+    if (url.length === 0) return;
+
+    const getData = async () => {
+      const res = await await axios.get(url);
+      setContent(res);
+    };
+
+    getData();
+  }, [url]);
+
+  console.log(url);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="md">
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          label="gateway to heaven"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment>
+                <IconButton onClick={handleClick}>
+                  <SearchIcon />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </form>
+      <div className={classes.terminal}><pre>{JSON.stringify(content, null, 2)}</pre></div>
+    </Container>
   );
 }
 
